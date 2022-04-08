8621aa0c1e1737eeafc5e7cc1ac42d4fc31d3a38
mario.mazzetti <mario.mazzetti@gmail.com>
2022-04-08 16:36:18 +0200

    prima commit

---
+++ src/main/java/it/infomed/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.infomed.helloworld;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ *
+ * @author mario
+ */
+public class HelloWorld {
+    
+    protected static final String HELLO_WORLD = "hello world.";
+    
+    public static void main(String[] args) {
+        System.out.println("Ciao Mondo");
+        
+        for (int i = 5; i < HELLO_WORLD.length(); i++) {
+            String sa = StringUtils.abbreviate(HELLO_WORLD, i);
+            System.out.println(i + " - " + sa);
+        }
+    }
+}
